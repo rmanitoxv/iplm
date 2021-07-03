adc20974fddd97f0792be02a30ff00116235ef98
Sean Cafe <seanamir30@gmail.com>
2021-07-03 11:58:02 +0800

    another commit

---
+++ CRS/forms.py
@@ -0,0 +1,31 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class InputGrades(ModelForm):
+	class Meta:
+		model = currchecklist
+		fields = ('curriculumCode', 'subjectGrades')
+
+class StudentsForm(forms.ModelForm):
+	class Meta:
+		model = StudentInfo
+		fields = 'studentUser','studentSection',
+		widgets = {
+			'studentUser': forms.Select(
+				attrs={"class": "form-control"}),
+
+			'studentSection': forms.Select(
+				attrs={
+					"class": "form-control",
+					"type": "text",
+					"placeholder": "1",
+					"id": "editprofile"
+				}
+			)
+		}
+
+class studyPlanForm(forms.ModelForm):
+	class Meta: 
+		model = studyPlan
+		fields = ['curricula']
