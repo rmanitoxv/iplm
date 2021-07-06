c53064bd9e80b8b1a884e5bf44eb474a8d1ba635
Sean Cafe <seanamir30@gmail.com>
2021-07-06 16:11:16 +0800

    final na talaga to

--- iPLMver2/settings.py
+++ iPLMver2/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
 }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -125,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
