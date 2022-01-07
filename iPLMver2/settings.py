659fae4e00640ad577e44ef44c9d472466d6fe3d
seanamir30 <47918193+seanamir30@users.noreply.github.com>
2022-01-07 21:38:15 +0800

    Update settings.py

--- iPLMver2/settings.py
+++ iPLMver2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sz68q!bdy_(f^!q%azzlt-s)r0_qb7e@jbh)7=68u!z*(i5+^n'
+SECRET_KEY = ENV['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
